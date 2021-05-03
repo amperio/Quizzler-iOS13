fdd49911da5d9652c8cdb05ef29fd4a7ff21bd6b
Juan Daniel <pericles_navarro@hotmail.com>
2021-05-03 09:49:49 -0500

    Add files via upload

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var txt_question: UILabel!
+    @IBOutlet weak var answ1: UIButton!
+    @IBOutlet weak var answ2: UIButton!
+    @IBOutlet weak var answ3: UIButton!
+    @IBOutlet weak var pgbEstado: UIProgressView!
+    @IBOutlet weak var score: UILabel!
+    
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    @IBAction func btn_answers(_ sender: UIButton) {
+        
+        let result = quizBrain.checkAnswer(userAnswer: sender.currentTitle!)
+        if result{
+            delayy(boton: sender, colorin: UIColor.green)
+        }
+        else{
+            delayy(boton: sender, colorin: UIColor.red)
+        }
+        
+        quizBrain.checkEnd()
+        updateUI()
+    }
+    
+    func updateUI() {
+        txt_question.text = quizBrain.getQuestion()
+        pgbEstado.progress = quizBrain.setProgresBarState()
+        score.text = quizBrain.getScore()
+        answ1.setTitle(quizBrain.getAnswers(indice: 0), for: .normal)
+        answ2.setTitle(quizBrain.getAnswers(indice: 1), for: .normal)
+        answ3.setTitle(quizBrain.getAnswers(indice: 2), for: .normal)
+    }
+    
+    func delayy(boton: UIButton, colorin: UIColor) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+            boton.backgroundColor = UIColor.clear
+        }
+        boton.backgroundColor = colorin
+    }
+}
+
---
+++ Quizzler-iOS13/Model/QuizModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizModel.swift
+//  Quizzler-iOS13
+//
+//  Created by Juan Navarro  on 4/7/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var q: String
+    var a: [String]
+    var correctAnswer: String
+}
